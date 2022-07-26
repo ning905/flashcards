63e62ee07e9fd7ff2f4e499ac9da7985643dc2b3
Ning Ma <ningma905@gmail.com>
2022-07-26 20:14:35 +0100

    finish studying page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import CardList from "./components/CardList/CardList";
 import Header from "./components/Header/Header";
 import StartButton from "./components/StartButton/StartButton";
